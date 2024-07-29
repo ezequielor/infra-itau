2acf7a11bcd208137ab7e4c7693f5cf710c441ce
ezequielor <eoliveira1991@hotmail.com>
2024-07-29 11:41:36 -0300

    Adiciona correção de provider no script

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,3 @@
-provider "aws" {
-  region = "us-east-1"  
-}
-
-resource "aws_instance" "application" {
-  ami = "ami-0427090fd1714168b"
-  instance_type = "t2.micro"
-
-  tags = {
-    "Name" = "Test Itau Application"
-  }
-}
-
-output "public_ip" {
-  value = aws_instance.application.public_ip
-}
-
 # Definindo o provedor AWS
 provider "aws" {
   region = "us-east-1"
