bbdc36013064aa61d9a1e5869e9c7b37bfd44e6a
ezequielor <eoliveira1991@hotmail.com>
2024-07-29 11:43:53 -0300

    Adiciona correção da configuração de subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"  # Altere para a zona de disponibilidade desejada
+  availability_zone       = "us-east-1a"  
   map_public_ip_on_launch = true
   tags = {
     Name = "main-subnet"
